1de3768a4e5adc940cb736d49c48e19f86e2e001
Rachel Lima <rachellima@ciandt.com>
2024-03-23 13:46:30 -0300

    added animations

--- app/src/main/java/com/rachellima/basiccompose/MainActivity.kt
+++ app/src/main/java/com/rachellima/basiccompose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,6 +22,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -63,7 +68,12 @@ fun MessageCard(msg: Message) {
                         .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
                 )
                 Spacer(modifier = Modifier.width(20.dp))
-                Column { // vertical
+                var isExpanded by remember { mutableStateOf(false) }
+
+                Column(modifier = Modifier.clickable {
+                    isExpanded = !isExpanded
+                }
+                ) { // vertical
                     Text(
                         text = msg.author,
                         color = MaterialTheme.colorScheme.secondary,
@@ -74,7 +84,10 @@ fun MessageCard(msg: Message) {
                         shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp
                     ) {
                         Text(
-                            text = msg.body, style = MaterialTheme.typography.bodyMedium
+                            text = msg.body,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.padding(4.dp),
+                            maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         )
                     }
                 }
