f44375ae383a9d2bdf9e25c3fbdb280e0b7b5da2
Rachel Lima <rachellima@ciandt.com>
2024-03-23 13:37:08 -0300

    added sample data for conversation

--- app/src/main/java/com/rachellima/basiccompose/MainActivity.kt
+++ app/src/main/java/com/rachellima/basiccompose/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -96,3 +98,19 @@ fun PreviewMessageCard() {
     )
 }
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    BasicComposeTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
